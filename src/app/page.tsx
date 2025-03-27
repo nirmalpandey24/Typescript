1716fac7b80976a114f1f659626414f79aa2b5a2
nirmalpandey24 <nirmalpandey055@gmail.com>
2025-03-27 09:02:15 +0545

    demo 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 export default function Home(){
 
+  type nirmal ={
+    name:string,
+    age:number,
+  }
+
+  function showname(person:nirmal):void{
+
+    console.log(`${person.name,person.age}`)
+  }
+
+  showname({
+     name:"nirmal",
+     age:5,
+  })
+
  return(
   <>
   <h1>Nirmal Pandey</h1>
   </>
  ) 
-}
+    }
